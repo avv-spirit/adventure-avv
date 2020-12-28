f408c157af915826f59dbb36a479bfe2ce10d345
avv-spirit <avvspirit@gmail.com>
2020-12-28 03:28:14 +0300

    Web page is under construction

--- source/js/script.js
+++ source/js/script.js
@@ -13,3 +13,23 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+const text = "Starting code...";
+let delay = 90;
+const elem = document.getElementById("code");
+
+let print_text = function(text, elem, delay) {
+  if(text.length > 0) {
+    elem.innerHTML += text[0];
+    setTimeout(
+      function() {
+        print_text(text.slice(1), elem, delay);
+      }, delay
+    );
+  }
+}
+print_text(text, elem, delay);
+setInterval(function() {
+  elem.innerHTML = "";
+  print_text(text, elem, delay);
+}, 8 * 1000);
